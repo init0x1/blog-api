fae2fcb8fb8e25bde268c6853a3963ae6e9847e5
0xAbdoAli <abdelrahman.elshaikh7@gmail.com>
2023-07-14 19:51:49 +0300

    installing db-migrate && db-migrate-pg && adding database configration json file && adding db-migrate test scripts

--- src/database.ts
+++ src/database.ts
@@ -8,13 +8,15 @@ const {
     POSTGRES_HOST,
     POSTGRES_PORT,
     POSTGRES_DB,
+    POSTGRES_DB_TEST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
+    NODE_ENV
 }= process.env
 
 const Client = new Pool({
     host:POSTGRES_HOST,
-    database:POSTGRES_DB,
+    database: NODE_ENV === 'dev' ? POSTGRES_DB : POSTGRES_DB_TEST,
     user:POSTGRES_USER,
     password:POSTGRES_PASSWORD,
     port:parseInt(POSTGRES_PORT as string),
